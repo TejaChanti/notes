a2be47ff23448aa2f8c3320118c02ecece80203f
TejaChanti <153368358+TejaChanti@users.noreply.github.com>
2024-07-29 13:36:27 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/notes';
+const API_URL = 'https://notes-backend-1u8o.onrender.com';
 
 export const getNotes = async () => {
   return await axios.get(API_URL);
